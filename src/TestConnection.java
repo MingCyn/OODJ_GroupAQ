1cc639d4bdf96783cd57d92c7252e20e058705a0
MingCyn <joslyn.cyn05@gmail.com>
2026-03-13 11:47:34 +0800

    Initial commit of mingcyn branches

--- src/TestConnection.java
+++ src/TestConnection.java
@@ -2,7 +2,7 @@
 public class TestConnection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git is linked!");
+		System.out.println("Git is linked!"); //test your connection here
 		System.out.println("This is a test for GitHub connection.");
 	}
 }
