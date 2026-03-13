805b3a8e2431d53034ba1d2a9141ae0435cb1ae9
y1iiiiiiii1 <jaimelai888@gmail.com>
2026-03-13 19:22:42 +0800

    test

---
+++ src/TestConnection2.java
@@ -0,0 +1,8 @@
+
+public class TestConnection2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is linked!");
+		System.out.println("This is a test for GitHub connection.");
+	}
+}
