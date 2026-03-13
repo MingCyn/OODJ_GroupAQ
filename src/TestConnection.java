ef5f3e33cb63d0b7541d29c07d45aa3410177b0c
y1iiiiiiii1 <jaimelai888@gmail.com>
2026-03-13 17:51:58 +0800

    Sinyi Test Connect

--- src/TestConnection.java
+++ src/TestConnection.java
@@ -3,6 +3,6 @@ public class TestConnection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is linked!");
-		
+		System.out.println("GIt is Linked test Sinyi");
 	}
 }
